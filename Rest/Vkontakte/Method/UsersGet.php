cda1624bba5999e14c19de7fb1528261f821a394
Sergey <go.for.broke1006@gmail.com>
2016-05-23 02:27:56 +0300

    Update UsersGet.php

--- Rest/Vkontakte/Method/UsersGet.php
+++ Rest/Vkontakte/Method/UsersGet.php
@@ -52,70 +52,6 @@ class UsersGet implements ApiMethodDescriptionInterface
     public function getParametersAllowedValues()
     {
         return array(
-            /*'fields' => [
-                'photo_id',
-                'verified',
-                'sex',
-                'bdate',
-                'city',
-                'country',
-                'home_town',
-                'has_photo',
-                'photo_50',
-                'photo_100',
-                'photo_200_orig',
-                'photo_200',
-                'photo_400_orig',
-                'photo_max',
-                'photo_max_orig',
-                'online',
-                'lists',
-                'domain',
-                'has_mobile',
-                'contacts',
-                'site',
-                'education',
-                'universities',
-                'schools',
-                'status',
-                'last_seen',
-                'followers_count',
-                'common_count',
-                'occupation',
-                'nickname',
-                'relatives',
-                'relation',
-                'personal',
-                'connections',
-                'exports',
-                'wall_comments',
-                'activities',
-                'interests',
-                'music',
-                'movies',
-                'tv',
-                'books',
-                'games',
-                'about',
-                'quotes',
-                'can_post',
-                'can_see_all_posts',
-                'can_see_audio',
-                'can_write_private_message',
-                'can_send_friend_request',
-                'is_favorite',
-                'is_hidden_from_feed',
-                'timezone',
-                'screen_name',
-                'maiden_name',
-                'crop_photo',
-                'is_friend',
-                'friend_status',
-                'career',
-                'military',
-                'blacklisted',
-                'blacklisted_by_me',
-            ],*/
             'name_case' => ['nom', 'gen', 'dat', 'acc', 'ins', 'abl'],
         );
     }
@@ -125,7 +61,17 @@ class UsersGet implements ApiMethodDescriptionInterface
      */
     public function getResultModelType()
     {
-        return 'array<GFB\RestClientBundle\Rest\Vkontakte\Model\User>';
-//        return 'GFB\RestClientBundle\Rest\Vkontakte\Model\Response';
+        return array(
+            array(
+                'from' => '*',
+                'to' => 'GFB\RestClientBundle\Rest\Vkontakte\Model\Response',
+                'to_var' => '*',
+            ),
+            array(
+                'from' => '*.response',
+                'to' => 'array<GFB\RestClientBundle\Rest\Vkontakte\Model\User>',
+                'to_var' => '*.response',
+            ),
+        );
     }
 }
